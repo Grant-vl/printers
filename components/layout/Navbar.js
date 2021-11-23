66f4be3385bdd9b0309cacf4e02626d6d7a7c346
grant-at-199487244040 <grantvleeuwen456@gmail.com>
2021-11-23 22:19:47 +0200

    Initial Commit

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -10,7 +10,7 @@ function Navbar () {
         <Header style={{height: '8vh', width: '100vw', padding:'0'}}>
             <Menu theme="dark" mode="horizontal" style={{height: '100%'}}>
                 <Link href="/" >
-                    <p style={{ paddingLeft:'5%', color: 'white', fontSize: '2rem',margin: 'auto', marginLeft:'0'}}>Printer App</p>
+                    <p style={{ paddingLeft:'5%', color: 'white', fontSize: '1.7rem',margin: 'auto', marginLeft:'0'}}>Printer App</p>
                 </Link>
             </Menu>
         </Header>
